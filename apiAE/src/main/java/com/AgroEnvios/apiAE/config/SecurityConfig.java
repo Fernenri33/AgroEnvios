4ea21fd08f0b8e3f66ce0eb6510f17202cd2d09a
fernenri <fernenri33@gmail.com>
2025-04-03 16:05:09 -0600

    Base de datos y primer endpoint

---
+++ apiAE/src/main/java/com/AgroEnvios/apiAE/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.AgroEnvios.apiAE.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/hola").permitAll() // Abre solo esta ruta
+                        .anyRequest().authenticated() // Protege las demás rutas
+                )
+                .build();
+    }
+}
