c979fc5be3700e387e5cea95993b87fa0fcf1e88
fernenri <fernenri33@gmail.com>
2025-04-28 09:15:16 -0600

    little cambios

--- agroenviosFront/src/components/Navbar.svelte
+++ agroenviosFront/src/components/Navbar.svelte
@@ -16,9 +16,9 @@
   <!-- Menú -->
   <div class={`absolute top-full left-0 w-full bg-[color:var(--color-background)] md:static md:flex md:w-auto md:space-x-10 font-bold font-serif transition-all duration-300 ease-in-out ${menuOpen ? 'block' : 'hidden'}`}>
     <a href="http://localhost:5173/#home" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Inicio</a>
-    <a href="#" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Servicios</a>
-    <a href="#" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Sobre Nosotros</a>
-    <a href="#" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Contáctanos</a>
+    <a href="http://localhost:5173" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Servicios</a>
+    <a href="http://localhost:5173" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Sobre Nosotros</a>
+    <a href="http://localhost:5173" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Contáctanos</a>
     <a href="/login" class="block px-6 py-2 md:px-0 md:py-0 hover:text-[color:var(--color-secondary)] transition">Iniciar Sesión</a>
   </div>
 </nav>
