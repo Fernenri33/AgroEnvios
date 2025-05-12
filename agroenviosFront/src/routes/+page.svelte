2976f26f110c57f12f133c223753f7337328da64
fernenri <fernenri33@gmail.com>
2025-05-12 08:52:43 -0600

    Añadiendo favicon a la página de inicio

--- agroenviosFront/src/routes/+page.svelte
+++ agroenviosFront/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 <svelte:head>
     <title>AgroEnvios</title>
     <meta name="Página de inicio" content="Bienvenido a AgroEnvios, su sistema para gestionar envíos, pedidos e inventario" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
 </svelte:head>
 
 
