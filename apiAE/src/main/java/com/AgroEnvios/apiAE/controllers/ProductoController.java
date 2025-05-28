529b38058a4442dad62c07be277cb15fe4864f11
fernenri <fernenri33@gmail.com>
2025-05-27 21:58:20 -0600

    fix en endpoint

--- apiAE/src/main/java/com/AgroEnvios/apiAE/controllers/ProductoController.java
+++ apiAE/src/main/java/com/AgroEnvios/apiAE/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.AgroEnvios.apiAE.Models.ApiResponse;
@@ -54,7 +55,10 @@ public class ProductoController {
     }
 
     @PostMapping("/actualizarProducto")
-    public ResponseEntity<ApiResponse<Producto>> actualizarProducto(@RequestHeader("Authorization") String authHeader, Producto producto) {
+    public ResponseEntity<ApiResponse<Producto>> actualizarProducto(
+        @RequestHeader("Authorization") String authHeader,
+        @RequestBody Producto producto // <-- ¡Esto es clave!
+    ) {
         String token = authHeader.replace("Bearer ", "");
 
         if (jwtUtil.hasRole(token, "Admin")) {
