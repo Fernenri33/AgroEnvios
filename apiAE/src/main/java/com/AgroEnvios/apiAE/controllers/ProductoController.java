c07599dc7b1cbf574cb0d8bc28cbf949ad408d67
fernenri <fernenri33@gmail.com>
2025-05-27 21:42:28 -0600

    fix en endpoint

--- apiAE/src/main/java/com/AgroEnvios/apiAE/controllers/ProductoController.java
+++ apiAE/src/main/java/com/AgroEnvios/apiAE/controllers/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,23 +66,26 @@ public class ProductoController {
         }
     }
 
-    @GetMapping("/producto/{id}")
-    public ResponseEntity<ApiResponse<Producto>> obtenerProductoPorId(@RequestHeader("Authorization") String authHeader, Integer id) {
-        String token = authHeader.replace("Bearer ", "");
+@GetMapping("/producto/{id}")
+public ResponseEntity<ApiResponse<Producto>> obtenerProductoPorId(
+    @RequestHeader("Authorization") String authHeader,
+    @PathVariable Integer id
+) {
+    String token = authHeader.replace("Bearer ", "");
 
-        if (jwtUtil.hasRole(token, "Admin") || jwtUtil.hasRole(token, "Proveedor") || jwtUtil.hasRole(token, "Supervisor")) {
-            Producto producto = productoService.obtenerProductoPorId(id);
-            if (producto != null) {
-                return ResponseEntity.ok(new ApiResponse<>(producto, "Producto encontrado"));
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(new ApiResponse<>(null, "Producto no encontrado"));
-            }
+    if (jwtUtil.hasRole(token, "Admin") || jwtUtil.hasRole(token, "Proveedor") || jwtUtil.hasRole(token, "Supervisor")) {
+        Producto producto = productoService.obtenerProductoPorId(id);
+        if (producto != null) {
+            return ResponseEntity.ok(new ApiResponse<>(producto, "Producto encontrado"));
         } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                    .body(new ApiResponse<>(null, "No tienes permisos para ver este producto"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse<>(null, "Producto no encontrado"));
         }
+    } else {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ApiResponse<>(null, "No tienes permisos para ver este producto"));
     }
+}
     
     
 }
